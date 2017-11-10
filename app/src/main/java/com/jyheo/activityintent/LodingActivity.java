772ab1620a7a187f4b38f67db14bc77ed7d4c019
여율 <31907528+yule93@users.noreply.github.com>
2017-11-11 02:44:56 +0900

    애니메이션 추가
    하려 했는데 잘 안되네용...

--- app/src/main/java/com/jyheo/activityintent/LodingActivity.java
+++ app/src/main/java/com/jyheo/activityintent/LodingActivity.java
@@ -1,5 +1,6 @@
 package com.jyheo.activityintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,11 +13,17 @@ public class LodingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        startActivity(new Intent(LodingActivity.this, MainActivity.class));
+        overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
+
+        finish();
+        overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
     }
 }
